aaf89ae2e366ec8d18e407d8412a1148ab00c322
Imanprokeren <98250257+Imanprokeren@users.noreply.github.com>
2022-02-05 07:45:02 +0700

    Update grupbot.js

--- shop/grupbot.js
+++ shop/grupbot.js
@@ -2,14 +2,11 @@
 const gcbotwa = () =>{
 	return`Join Aja Semua Fitur Bot Bisa Digunakan !
 
-1. *Alphabot Support*
+1. *ImanBot Support*
 _https://chat.whatsapp.com/EU890BcXjyBDkNaUT5WmYV_
-2. *Alphabot Support 2*
-_https://chat.whatsapp.com/E8NExJwIbhBJYzssfqJNsE_
-3. *Alphabot Support 3*
-_https://chat.whatsapp.com/KCSqHTky1apG7ApePsfiPy_
-4. *Alphabot Support 4* 
-_https://chat.whatsapp.com/KwmvHr7VMFj7r5ry9xmMsU_
+2. *Nimvers*
+_https://chat.whatsapp.com/LJTyb2QShYYE8ckqKSqUPA
+
 
 Jika ada link yang ke reset, silahkan hubungi
 owner untuk meminta link yang baru
